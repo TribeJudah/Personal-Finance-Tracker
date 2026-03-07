660a99da83d41c6ae3d363775f6ea8bb5a89f0b2
Terry Jumah <Jehovahjay@gmail.com>
2026-03-07 21:10:50 +0000

    Remove old contents from finance-tracker-app

--- finance-tracker-app/src/components/Layout/Container.jsx
+++
@@ -1,9 +0,0 @@
-const Container = ({ children }) => {
-  return (
-    <div style={{ maxWidth: "1200px", margin: "0 auto", padding: "20px" }}>
-      {children}
-    </div>
-  );
-};
-
-export default Container;
